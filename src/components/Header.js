cbc1b0d850b7aa4d6158c9594c81f8934dcccee1
Ajay Bansal <120590200+abansal15@users.noreply.github.com>
2023-10-01 21:32:25 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// Jai shree ram
+import React from "react"
+import '../style.css'
+import logo from '../images/troll-face.png';
+export default function Header() {
+    return (
+        <div className="header">
+            <img className="header--image" src={logo} />
+            <h1 className="header--title">Meme Generator</h1>
+        </div>
+    )
+}
+// src\images\troll-face.png
